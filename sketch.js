6c898aa356a68523ecacb537f8a00915acf93ded
Ishpreet <71171946+ishpreet09@users.noreply.github.com>
2020-12-16 14:46:43 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,63 +1,50 @@
-var canvas;
-var backgroundImage, track, car1_img, car2_img;
-var fireAuth, db;
-var game, welcome, teacher, student;
-var secret_word;
-var player, allPlayers;
-var gameState = null;
+var canvas, backgroundImage;
+
+var gameState = 0;
 var playerCount;
-function preload() {
-  backgroundImage = loadImage("./assets/bg.jpg");
-  track = loadImage("./assets/track.jpg");
-  car1_img = loadImage("./assets/car1.png");
-  car2_img = loadImage("./assets/car2.png");
+var allPlayers;
+var distance = 0;
+var database;
+var finishedPlayers =0;
+var form, player, game;
+var passedFinish;
+var cars, car1, car2, car3, car4;
+
+var track, car1_img, car2_img, car3_img, car4_img;
+
+function preload(){
+  track = loadImage("images/track.jpg");
+  car1_img = loadImage("images/car1.png");
+  car2_img = loadImage("images/car2.png");
+  car3_img = loadImage("images/car3.png");
+  car4_img = loadImage("images/car4.png");
+  ground = loadImage("images/ground.png");
+  bronze_img = loadImage("images/bronze.png");
+  silver_img = loadImage("images/silver.png");
+  gold_img = loadImage("images/gold.png");
 }
 
-function setup() {
-  canvas = createCanvas(displayWidth, displayHeight);
-  fireAuth = firebase.auth();
-  db = firebase.database();
+function setup(){
+  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  database = firebase.database();
   game = new Game();
-  welcome = new Welcome();
-  teacher = new Teacher();
-  student = new Student();
-  player = new Player();
-
-  car1 = createSprite(width / 2, 200);
-  car1.addImage("car1", car1_img);
-  car2 = createSprite(width - 300, 200);
-  car2.addImage("car2", car2_img);
-
-  cars = [car1, car2];
+  game.getState();
+  game.start();
 }
 
-function draw() {
-  background(backgroundImage);
-  if (gameState === null || gameState === 0) {
-    game.start();
-  }
-  if (playerCount === 2) {
+function draw(){
+  background(200, 200, 255);
+  if(playerCount === 4 && finishedPlayers === 0){
     game.update(1);
   }
-
-  if (gameState === 1) {
+  if(gameState === 1){
     clear();
-    student.greeting.hide();
-    student.greeting2.hide();
-    student.playButton.hide();
-
-    teacher.greeting.hide();
-    teacher.greeting2.hide();
-    teacher.playButton.hide();
-    teacher.secretWord.hide();
-
     game.play();
   }
-  if (gameState === 2) {
-    game.end();
+  if(finishedPlayers === 4){
+    game.update(2);
   }
-}
-
-function windowResized() {
-  resizeCanvas(displayWidth, displayHeight);
+  if(gameState === 2 && finishedPlayers ===4){
+    game.displayRanks();
+  }
 }
